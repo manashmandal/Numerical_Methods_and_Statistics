765a88d3fb68bbec37bea377d3f0072a86ea9e84
Manash Mandal <manashmndl@gmail.com>
2015-06-08 13:17:51 +0600

    updated AM, GM, HM

--- Lab01/DataGrouping/main.cpp
+++ Lab01/DataGrouping/main.cpp
@@ -14,6 +14,16 @@ struct data
     int frequency = 0;
 };
 
+struct dataOperationResults
+{
+    double arithmatic_mean;
+    double geometric_mean;
+    double harmonic_mean;
+    double mode;
+
+    vector <double> median;
+};
+
 int main()
 {
 
@@ -22,11 +32,17 @@ int main()
    cin >> number;
 
     vector <int> a;
+    int sum = 0;
+    long long int multiplication = 1;
+    double inverse_sum = 0;
+
     int x;
     for (int i = 0 ; i < number; i++){
         cin >> x;
         a.push_back(x);
     }
+
+    dataOperationResults operated_data;
 
 
     int minVal =  *min_element(a.begin(), a.end());
@@ -43,16 +59,26 @@ int main()
     interVal = (double) (maxVal - minVal) / classNumber;
 
 
+    //Calculate total
+    for (size_t i = 0; i < a.size(); i++){
+        sum += a[i];
+    }
+
+    //Calculate multiplication
+    for (size_t i = 0; i < a.size(); i++){
+        multiplication *= a[i];
+    }
+
+    //Calculate Inverse sum
+    for (size_t i = 0; i < a.size(); i++){
+        inverse_sum += pow(a[i], -1);
+    }
+
+
     int left = minVal;
     int right = minVal + ceil(interVal);
 
-    int j = 1;
-
-
     int interval = ceil(interVal);
-
-//    classArray = new int[classNumber];
-//    classArrayUpper = new int[classNumber];
 
     //Class Creation
     for (int i = 0; i < classNumber; i++){
@@ -62,19 +88,13 @@ int main()
         right = left + interval;
     }
 
-
     // grouped data with frequency
-
     for (int i = 0 ; i < a.size(); i++){
-
         for (int j = 0; j < classNumber; j++){
-
             if (a[i] >= my_data[j].lowerValue && a[i] <= my_data[j].upperValue){
                 my_data[j].frequency++;
             }
-
         }
-
     }
 
 
@@ -87,20 +107,25 @@ int main()
     }
 
 
+    //Calculate Arithmatic Mean
+    operated_data.arithmatic_mean = static_cast<double> ( sum / a.size());
 
+    //Calculate Geometric Mean
+    operated_data.geometric_mean = static_cast<double> (pow(multiplication, 1.0 / a.size()));
 
+    //Calculate Harmonic Mean
+    operated_data.harmonic_mean = static_cast<double>(a.size() / inverse_sum);
 
+    cout << "Arithmatic mean: " << operated_data.arithmatic_mean << endl;
 
+    cout << "Geometric Mean: " << operated_data.geometric_mean << endl;
+
+    cout << "Harmonic Mean: " << operated_data.harmonic_mean << endl;
 
 //    int size;
 
 //    ifstream file("data.txt");
-//    //ofstream output("sheldon_cooper.txt");
-
-
 //    int k[2][2];
-
-
 //    for (int i = 0; i < 2; i++){
 //        for (int j = 0; j < 2; j++){
 //            file >> k[j][i];
